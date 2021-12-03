c24a7d69a1452e6831b207e1f0c4ca2e3140febc
romanbatavi <romanbatavi98@gmail.com>
2021-12-03 15:10:00 +0700

    training wizard : creating listDawdasdadwdsagfdjhfd

--- training_odoo/wizard/training_wizard.py
+++ training_odoo/wizard/training_wizard.py
@@ -1,13 +1,19 @@
 from odoo import api, fields, models
+
 class TrainingWizard(models.TransientModel):
     _name = 'training.wizard'
     _description = 'Training Wizard'
   
     def _default_sesi(self):
         return self.env['training.session'].browse(self._context.get('active_ids'))
-      
+    
+    session_ids = fields.Many2many('training.session', string="Sesi", default=_default_sesi)
     session_id = fields.Many2one('training.session', string="Sesi", default=_default_sesi)
     attendee_ids = fields.Many2many('training.attendee', string='Peserta')
      
     def tambah_peserta(self):
         self.session_id.attendee_ids |= self.attendee_ids
+    
+    def tambah_banyak_peserta(self):
+        for sesi in self.session_ids:
+            sesi.attendee_ids |= self.attendee_ids
