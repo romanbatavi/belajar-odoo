ad9f0a13f2605156421cd89a41b0f5fe71e7e75e
romanbatavi <romanbatavi98@gmail.com>
2021-12-06 11:43:43 +0700

    get rest travel

--- training_odoo2/__manifest__.py
+++ training_odoo2/__manifest__.py
@@ -25,7 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/interface.xml',
+        'views/home.xml',
+        # 'views/umroh_produk.xml',
         'views/views.xml',
         'views/templates.xml',
         
